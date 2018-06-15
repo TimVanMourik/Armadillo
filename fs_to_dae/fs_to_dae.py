16cb453800f5179028df90aa10c48697f143d1ef
dcmoyer <dcmoyer@gmail.com>
2018-06-14 18:52:41 -0700

    readded normals

--- fs_to_dae/fs_to_dae.py
+++ fs_to_dae/fs_to_dae.py
@@ -1,11 +1,40 @@
 
 from nibabel.freesurfer import io as fsio
 import collada
+import numpy as np
 
 def fs_to_dae( args ):
 
   #load in FS mesh
   verts,faces = fsio.read_geometry( args.input )
+
+  #dumb copypasta for mesh face normals
+  norms = np.zeros( verts.shape, dtype=verts.dtype )
+  tris = verts[faces]
+  n = np.cross( tris[::,1 ] - tris[::,0]  , tris[::,2 ] - tris[::,0] )
+  norm_sizes = np.sqrt(n[:,0]**2 + n[:,1]**2 + n[:,2]**2)
+
+  for i in range(3):
+    n[:,i] = n[:,i] / norm_sizes
+
+  del norm_sizes
+
+  #map back to vertices
+  norms[ faces[:,0] ] += n
+  norms[ faces[:,1] ] += n
+  norms[ faces[:,2] ] += n
+
+  del n
+
+  norm_sizes = np.sqrt(norms[:,0]**2 + norms[:,1]**2 + norms[:,2]**2)
+
+  for i in range(3):
+    norms[:,i] = norms[:,i] / norm_sizes
+
+
+  #
+  # collada section
+  #
 
   #create collada obj
   mesh = collada.Collada()
@@ -19,16 +48,19 @@ def fs_to_dae( args ):
   mesh.materials.append(mat)
 
   vert_src = collada.source.FloatSource("cubeverts-array", verts, ('X', 'Y', 'Z'))
+  norm_src = collada.source.FloatSource("cubenormals-array", np.array(norms), ('X', 'Y', 'Z'))
 
-  geom = collada.geometry.Geometry(mesh, "geometry0", "fsave_test", [vert_src])
+  geom = collada.geometry.Geometry(mesh, "geometry0", "fsave_test", [vert_src,norm_src])
 
   #creates list of inputs for collada DOM obj...so many decorators 
   input_list = collada.source.InputList()
 
   input_list.addInput(0, 'VERTEX', "#cubeverts-array")
+  input_list.addInput(1, 'NORMAL', "#cubenormals-array")
 
   #creates faces
-  triset = geom.createTriangleSet(faces, input_list, "materialref")
+  triset = geom.createTriangleSet(
+    np.array([faces,faces]), input_list, "materialref")
 
   geom.primitives.append(triset)
   mesh.geometries.append(geom)
