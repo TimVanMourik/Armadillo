afb92e4833e504c345f7f3cc618ec3834c88ba69
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-06-17 00:02:27 +0800

    actually loading the neurovault files now

--- app/armadillo/models.py
+++ app/armadillo/models.py
@@ -3,18 +3,21 @@ from django.contrib.postgres.fields import JSONField
 from django.core.files.base import ContentFile
 from django.conf import settings
 from urllib.request import urlopen
+import urllib
 import urllib.request, json
 import base64
 import os
 from django.http import HttpResponse
-from urllib.request import urlopen
 import nibabel as nib
 from nibabel.freesurfer import io as fsio
+from xml.dom.minidom import parse, parseString
+import numpy
+import io
+import zlib
 
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
-from .utils import create_qr_from_text, put_qr_on_marker, color_func, fv_scalar_to_collada
-
+from .utils import create_qr_from_text, put_qr_on_marker, color_func, fv_scalar_to_collada, gunzip_bytes_obj
 
 def qr(request, image=''):
     qr_link = os.path.join(settings.BASE_URL, 'neurovault/'+ image)
@@ -32,8 +35,12 @@ def hemisphere(request, image='', hemisphere=''):
         fileData = None
 
     surface = fileData[f"surface_{hemisphere}_file"]
-    with urllib.request.urlopen(surface) as response:
-        gii = response.read().decode()
+
+    dom = parse(urllib.request.urlopen(surface))
+    zip_base64 = dom.getElementsByTagName('Data')[0].childNodes[0].data
+    zip = base64.b64decode(zip_base64.encode('ascii'))
+    unzip = zlib.decompress(zip)
+    colors = numpy.fromstring(unzip, dtype='float32')
 
     if hemisphere not in ["left","right"]:
         print("BAD HEMI IN IMAGE FUNC")
@@ -43,18 +50,10 @@ def hemisphere(request, image='', hemisphere=''):
     else:
         hemi_short = "rh"
 
-    # THIS IS A COMPLETE HACK
-    # TODO: remove this
-    # with open("temp.func.gii","w") as f:
-       # f.write(gii)
-    scalars = nib.load( os.path.join(settings.BASE_DIR, f'staticfiles/fs/{hemi_short}.func.gii'))
-    #scalars = nib.load(f'/neurovault/{image}/gifti/{hemisphere}')
-    scalars = scalars.darrays[0].data
     fs_base = os.path.join(settings.BASE_DIR, 'staticfiles/fs/')
-
     verts,faces = fsio.read_geometry(os.path.join(fs_base,"%s.pial" % hemi_short))
 
-    bytestream = bytes(fv_scalar_to_collada(verts,faces,scalars).getvalue())
+    bytestream = bytes(fv_scalar_to_collada(verts,faces,colors).getvalue())
     a  = ContentFile(bytestream, f"{hemisphere}.dae")
     file = InMemoryUploadedFile(a, None, f"{hemisphere}.dae", 'application/xml', len(bytestream), None)
     return HttpResponse(file, content_type='application/xml')
@@ -71,4 +70,4 @@ def gifti(request, image='', hemisphere=''):
 
     surface = fileData[f"surface_{hemisphere}_file"]
 
-    return HttpResponse(urlopen(surface), content_type='application/xml')
+    return HttpResponse(urllib.request.urlopen(surface), content_type='application/xml')
--- app/armadillo/utils.py
+++ app/armadillo/utils.py
@@ -8,6 +8,7 @@ from django.conf import settings
 import collada
 import numpy as np
 import io
+import gzip
 
 DEF_SIZE_MARKER = 512
 
@@ -107,3 +108,21 @@ def fv_scalar_to_collada(verts,faces,scalars):
     buf = io.BytesIO()
     mesh.write(buf)
     return buf
+
+def gzip_str(string_):
+    out = io.BytesIO()
+
+    with gzip.GzipFile(fileobj=out, mode='w') as fo:
+        fo.write(string_.encode())
+
+    bytes_obj = out.getvalue()
+    return bytes_obj
+
+def gunzip_bytes_obj(bytes_obj):
+    in_ = io.BytesIO()
+    in_.write(bytes_obj)
+    in_.seek(0)
+    with gzip.GzipFile(fileobj=in_, mode='rb') as fo:
+        gunzipped_bytes_obj = fo.read()
+
+    return gunzipped_bytes_obj.decode()
