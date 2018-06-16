b01dd6abca9813f7400a8661ec14b4b86a666548
dcmoyer <dcmoyer@gmail.com>
2018-06-15 21:32:06 -0700

    wrapping gii into dae

--- app/armadillo/views.py
+++ app/armadillo/views.py
@@ -6,9 +6,83 @@ from django.templatetags.static import static
 from PIL import Image
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
+import nibabel as nib
+from nibabel.freesurfer import io as fsio
+import os
+from django.conf import settings
+import collada
+import numpy as np
+
+import io
+
 def index(request):
     context = {}
     return TemplateResponse(request, 'index.html', context)
+
+#todo: use matplotlib color functions
+def color_func( scalars ):
+    smin = np.min(scalars)
+    smax = np.max(scalars)
+    scalars = (scalars - smin) / smax
+  
+    colors = np.zeros((scalars.shape[0],3))
+    colors[:,1] += scalars
+    colors[:,2] += (1-scalars)
+  
+    return colors
+
+
+def fv_scalar_to_collada(verts,faces,scalars):
+
+    color = color_func(scalars)
+
+    #create collada obj
+    mesh = collada.Collada()
+
+    #add shading
+    effect = collada.material.Effect("effect0",\
+      [], #TEXTURES GO HERE
+      "phong", diffuse=(1,1,1), specular=(1,1,1),
+      double_sided=True)
+    mat = collada.material.Material("material0", "mymaterial", effect)
+    mesh.effects.append(effect)
+    mesh.materials.append(mat)
+
+    vert_src = collada.source.FloatSource("verts-array", verts, ('X', 'Y', 'Z'))
+    color_src = collada.source.FloatSource("colors-array", np.array(color), ('R', 'G', 'B'))
+
+    geom = collada.geometry.Geometry(mesh, "geometry0", "fsave_test",\
+      [vert_src,color_src])
+
+    #creates list of inputs for collada DOM obj...so many decorators 
+    input_list = collada.source.InputList()
+
+    input_list.addInput(0, 'VERTEX', "#verts-array")
+    input_list.addInput(1, 'COLOR', "#colors-array")
+
+    #creates faces
+    triset = geom.createTriangleSet(
+      np.concatenate([faces,faces],axis=1),\
+      input_list, "materialref")
+
+    triset.generateNormals()
+
+    geom.primitives.append(triset)
+    mesh.geometries.append(geom)
+
+    #creates scene node, which causes display
+    matnode = collada.scene.MaterialNode("materialref", mat, inputs=[])
+    geomnode = collada.scene.GeometryNode(geom, [matnode])
+    node = collada.scene.Node("node0", children=[geomnode])
+
+    #create scene
+    myscene = collada.scene.Scene("fs_base_scene", [node])
+    mesh.scenes.append(myscene)
+    mesh.scene = myscene
+
+    buf = io.BytesIO()
+    mesh.write(buf)
+    return buf
 
 def image(request, image=''):
     # query neurovault image
@@ -22,8 +96,26 @@ def image(request, image=''):
     # parse image
 
     # colour file
-    # surface_left_file =  fileData['surface_left_file']
-    # surface_right_file = fileData['surface_right_file']
+    surface_left_file =  fileData['surface_left_file']
+    surface_right_file = fileData['surface_right_file']
+
+    with urllib.request.urlopen(surface_left_file) as response:
+        gii = response.read().decode()
+
+    #THIS IS A COMPLETE HACK
+    #TODO: remove this
+    with open("temp.func.gii","w") as f:
+        f.write(gii)
+    lh_scalars = nib.load("temp.func.gii")
+    lh_scalars = lh_scalars.darrays[0].data
+    ##print(lh_scalars.darrays[0].data)
+
+    #nib.load("/neurovault/{image}/lh")
+
+    fs_base = os.path.join(settings.BASE_DIR, 'staticfiles/fs/')
+    lh_v,lh_f = fsio.read_geometry(os.path.join(fs_base,"lh.pial"))
+
+    print(fv_scalar_to_collada(lh_v,lh_f,lh_scalars))
 
     # if surface_left_file != None & surface_right_file != None
 
