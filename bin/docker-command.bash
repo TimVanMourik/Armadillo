520c64d43daac5301068ed8ab20e64c74d046c80
TimVanMourik <timvanmourik@gmail.com>
2018-07-04 21:46:07 +0200

    restructure for travis

--- bin/docker-command.bash
+++ bin/docker-command.bash
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# The next block will read your .env
-if [ -f .env ]; then
-    export $(cat .env | grep -v ^# | xargs)
+# Hello there!
+# If you want to use your own environment settings, please copy:
+#   .env.sample --> .env
+source ./bin/init-environment.bash
+
+# initialise django
+bash ./bin/init-django.bash
+
+# watch changes and live reload them on changes:
+if [ "$MODE" = "watch" ]; then
+  bash ./bin/init-watch.bash
 fi
 
-#django commands
+# run server
 cd app
-python manage.py collectstatic --noinput -i other &
-python manage.py migrate
-python manage.py livereload --host 0.0.0.0 --port 35729 &
 python manage.py runserver 0.0.0.0:8000
